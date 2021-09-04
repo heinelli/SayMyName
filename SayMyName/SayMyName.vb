0e8a7f36e4f192167aa9bf91ab88af439ffbc44a
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-03 18:32:58 -0600

    Add project files.

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,17 @@
+﻿'Elliot Heiner
+'RCET0265
+'Fall 2021
+'Say My Name
+'
+Module SayMyName
+
+    Sub Main()
+        Dim name As String
+        Console.WriteLine("Hello there. What's your name?")
+        name = Console.ReadLine()
+        Console.Write("Hello there " & name)
+        Console.Read()
+
+    End Sub
+
+End Module
