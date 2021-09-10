528e7e68f831b72c6891bc64b23447d8c38eeb6e
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-10 11:10:32 -0600

    Add custom responses for individual names.

--- SayMyName/SayMyNameAgain.vb
+++ SayMyName/SayMyNameAgain.vb
@@ -6,19 +6,20 @@
 
 Option Explicit On
 Option Strict On
+Option Compare Text
 Module SayMyNameAgain
     Sub Main()
         Dim name As String  'A variable called "name" is created
         Console.WriteLine("Hello there. What's your name?")
         name = Console.ReadLine()   'User input is saved as the variable "name"
-        If name = "Joe" Then
-            Console.WriteLine("Hello there " & name & ". What are you doing?")
+        If name = "Joe" Or name = "Emily" Then
+            Console.WriteLine("Hello there " & name & ". It's good to see you.")
+        ElseIf name = "Elliot" Then
+            Console.WriteLine("Bom dia Elliot! Tudo bem?")
         Else
             Console.WriteLine("Oh no. Not you again!")
         End If
-
-        Console.ReadLine()  'Creates delay so that user can type response
-        Console.Write("That sounds boring! " & name & ", can you press ENTER please so that I can go back to sleep?")
+        Console.WriteLine("Press ENTER to quit")
         Console.Read()  'Creates delay so that user can press ENTER.
     End Sub
 
